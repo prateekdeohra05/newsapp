2ad4c24e9d18d77f64dc6164677b23c46685beb3
Prateek Sharma <tikupandit05@gmial.com>
2021-10-11 16:50:55 +0530

    commit on 'master'

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import InfiniteScroll from "react-infinite-scroll-component"; 
 
 export class News extends Component {
-    render() {
+    
+    constructor(){
+        super();
+        this.state={
+            articles:[],
+            page:1,
+            totalResults:0
+        }
+        }
+      async componentDidMount(){
+        this.props.setProgress(10);
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}`
+            let data = await fetch(url);
+            this.props.setProgress(60);
+            let parsedData = await data.json();
+            this.setState({
+              articles : parsedData.articles,
+              totalResults: parsedData.totalResults
+            })
+            this.props.setProgress(100);
+        
+        }
+      fetchMoreData=async()=>{
+          this.setState({page:this.state.page + 1})
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles : this.state.articles.concat(parsedData.articles),
+           totalResults: parsedData.totalResults
+        })
+      console.log(this.state.articles)
+      console.log(this.state.totalResults)
+      }
+     
+      
+      
+      render() {
+       
         return (
-            <div>
-               This is a news component.
-               <NewsItem/> 
+            <>
+            <h2 className="text-center" style={{marginTop:'80px'}}>Khabri - Top Headlines</h2>
+            <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length!==this.state.totalResults}
+          loader={<Spinner/>}
+        >
+            <div className='container my-3'>
+              <div className="row">
+                {this.state.articles.map((element)=>{   
+                return <div className="col-md-4" key={element.url}>
+               <NewsItem  title={element.title} description ={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} time={element.publishedAt} source={element.source.name}/> 
+                  </div>
+                })}
             </div>
+               </div>
+               </InfiniteScroll>
+</>
         )
     }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
+        let  {title, description, imageUrl,newsUrl, author, time, source}= this.props;
         return (
-            <div>
-                NewsItem
+            <div >
+     <div className="card" >
+  <img src= {!imageUrl?"https://photo-cdn2.icons8.com/Nx7dzZwQfa2K079AyAPZGpTH1g4jYR-oVJY25TO-7bQ/rs:fit:1606:1072/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5h/c3NldHMvYXNzZXRz/L3NhdGEvb3JpZ2lu/YWwvNjIxL2QzMWUx/OWZhLTcyOTEtNGU3/Ni1iZmMzLTRhZmE1/NzdiYzliNy5qcGc.jpg":imageUrl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+  <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1'}}>
+    {source}
+  </span>
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <p className="card-text"><small className="text-muted">Published {author===null?"":"by"} {author} {time===null?"":"on"} {time===null?"":`${new Date(time).toGMTString()}`}</small></p>
+    <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+  </div>
+</div>
             </div>
         )
     }
